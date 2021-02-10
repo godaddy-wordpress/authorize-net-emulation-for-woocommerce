22244184fc8443855c59d0990445117c3835adae
Illimar Tambek <itambek@godaddy.com>
2021-02-10 11:55:10 +0200

    [MWC-43] Create the plugin loader and the main function

---
+++ includes/Functions.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Authorize.Net Emulation for WooCommerce
+ *
+ * This source file is subject to the GNU General Public License v3.0
+ * that is bundled with this package in the file license.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@skyverge.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Authorize.Net Emulation for WooCommerce to newer
+ * versions in the future. If you wish to customize Authorize.Net Emulation for WooCommerce for your
+ * needs please refer to http://www.woocommerce.com/products/ for more information. TODO: docs url
+ *
+ * @author      SkyVerge
+ * @copyright   Copyright (c) 2021, SkyVerge, Inc.
+ * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
+ */
+
+/**
+ * Gets the One True Instance of Authorize.Net Emulation.
+ *
+ * @return \SkyVerge\WooCommerce\Authorize_Net\Emulation
+ * @since 1.0.0
+ *
+ */
+function wc_authorize_net_emulation()
+{
+
+    return \SkyVerge\WooCommerce\Authorize_Net\Emulation\Plugin::instance();
+}
+
